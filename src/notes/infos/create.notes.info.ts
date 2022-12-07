f6d75ffe67d16e0544eb91a7141e4834cd720034
Toquero <toquerochristy08@gmail.com>
2022-12-07 16:23:26 +0800

    Added Validation here

--- src/notes/infos/create.notes.info.ts
+++ src/notes/infos/create.notes.info.ts
@@ -1,4 +1,13 @@
-export class CreateNotesInfo{
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateNotesInfo {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
     title: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(150)
     description: string;
 }
